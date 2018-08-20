9320a2aea807f332e4126c78674a2d3611554289
saruatchu <sarumathiayyappan@gmail.com>
2018-08-20 14:04:33 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,13 @@
+# new1
+import java.io.*;
+class Largest
+{
+int a,b,c;
+System.out.println("Enter the three numbers");
+if(a>b&&a>c)
+System.out.println("A is greater");
+else if
+System.out.println("b is greater");
+else
+System.out.println("c is greater");
+}
